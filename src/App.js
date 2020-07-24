57267d2727ac43401ff20b4b73ce23951b2fc6c9
jmtd25 <jdimapilis.ucamp@gmail.com>
2020-07-24 15:44:16 +0800

    Scaffold react navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+        <br />
+        <Switch>
+          <Route path="/services">Services</Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
